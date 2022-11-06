64cc6f51c4b9c373bdff0512402edfbdd9aaedd8
Tanu <tanupandey291099@gmail.com>
2022-11-06 22:24:12 +0530

    added DOM methods

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,60 +1,79 @@
-let ccounter=0;
-let ucounter=0;
-const choices=["rock","paper","scissor"];
+let ccounter = 0;
+let ucounter = 0;
+const choices = ["rock", "paper", "scissor"];
 
 
-function getComputerChoice(choice)  
-    {return Math.floor(Math.random()*choice.length);
+function getComputerChoice(choice) {
+    return Math.floor(Math.random() * choice.length);
 
 }
 
 
- function playRound(playerSelection,computerSelection)
- {
-    
-    if(computerSelection=="rock"&&playerSelection=="scissor")
-    {ccounter++;
-    return (`You lose !! ${computerSelection} beats ${playerSelection}`);
-    
-}
-    else if(computerSelection=="scissor"&&playerSelection=="paper")
-    {ccounter++;
-    return (`You lose !! ${computerSelection} beats ${playerSelection}`);
-    
-    }
-   else if(computerSelection=="paper"&&playerSelection=="rock")
-   {ccounter++;
-    return (`You lose !! ${computerSelection} beats ${playerSelection}`);
-    
-   }
-    else if(computerSelection===playerSelection)
-    return (`Its a tie`);
-    else 
-    {ucounter++;
-    return (`You win !! ${playerSelection} beats ${computerSelection}`);
+function playRound(playerSelection, computerSelection) {
+
+    if (computerSelection == "rock" && playerSelection == "scissor") {
+        ccounter++;
+        div.textContent = `You lose !! ${computerSelection} beats ${playerSelection} `;
+        div.textContent += `You:${ucounter} Computer:${ccounter}`
 
     }
+    else if (computerSelection == "scissor" && playerSelection == "paper") {
+        ccounter++;
+        div.textContent = `You lose !! ${computerSelection} beats ${playerSelection} `;
+        div.textContent += `You:${ucounter} Computer:${ccounter}`
+
+    }
+    else if (computerSelection == "paper" && playerSelection == "rock") {
+        ccounter++;
+        div.textContent = `You lose !! ${computerSelection} beats ${playerSelection} `;
+        div.textContent += `You:${ucounter} Computer:${ccounter}`
+
+    }
+    else if (computerSelection === playerSelection) {
+        div.textContent = `Its a tie `;
+        div.textContent += `You:${ucounter} Computer:${ccounter}`
+    }
+    else {
+        ucounter++;
+        div.textContent = `You win !! ${playerSelection} beats ${computerSelection} `;
+        div.textContent += `You:${ucounter} Computer:${ccounter}`
+
+    }
+    if (ccounter === 5 || ucounter === 5) {
+        let finalResult = document.createElement('div');
+        if (ccounter > ucounter)
+            finalResult.textContent = `Computer win`;
+        else
+            finalResult.textContent = `You win`;
+        ccounter = 0;
+        ucounter = 0;
+        document.body.appendChild(finalResult);
+
+    }
+
 
 }
-
-function game()
-{   setInterval(function(){
-      let userChoice=prompt("enter your choice ").toLowerCase();
-    let computerChoice=choices[getComputerChoice(choices)];
+//older version of rps used this function
+/*function game() {
+    //for (let i = 1; i <= 5; i++) {
+    let userChoice = prompt("enter your choice ").toLowerCase();
+    let computerChoice = choices[getComputerChoice(choices)];
     console.log(computerChoice);
     console.log(userChoice);
-    const result=playRound(userChoice,computerChoice);
+    const result = playRound(userChoice, computerChoice);
     console.log(result);
     console.log(ccounter);
     console.log(ucounter);
 
-},5000);
-}
 
-for (let i=1;i<=5;i++)
-{ let play=prompt("Do you want to play?").toLowerCase();
-if(play==="yes")
-game();
-else if(play==="no")
-break;
-}
+
+}*/
+let buttons = document.querySelectorAll('button');
+
+for (let i = 0; i < buttons.length; i++)
+
+    buttons[i].addEventListener('click', function (e) {
+        playRound(e.target.value, choices[getComputerChoice(choices)])
+    });
+let div = document.querySelector('div');
+
